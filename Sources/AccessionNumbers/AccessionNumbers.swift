6f4be92d845df58a9c44788eb1b2a061788c8736
thisisaaronland <aaron@localhost>
2021-11-15 13:42:50 -0800

    snapshot: start blocking out code; incomplete and untested

--- Sources/AccessionNumbers/AccessionNumbers.swift
+++ Sources/AccessionNumbers/AccessionNumbers.swift
@@ -1,8 +1,8 @@
-public struct AccessionNumbers {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
-    }
+public struct AccessionNumber: Codable {
+    var Organization: String
+    var Number: String
 }
 
 public struct Pattern: Codable {
@@ -15,5 +15,79 @@ public struct Organization: Codable {
     var Name: String
     var URL: String
     var Patterns: [Pattern]
+}
+
+public struct AccessionNumbers {
+    
+    private var candidates: [Organization]
+    
+    public init(candidates: [Organization]) {
+        self.candidates = candidates
+    }
+    
+    public func ExtractFromText(text: String) -> Result<[AccessionNumber], Error> {
+        
+        var accession_numbers = [AccessionNumber]()
+        
+        for org in self.candidates {
+            
+            let rsp = self.ExtractFromTextWithOrganization(text: text, organization: org)
+            
+            switch rsp {
+            case .failure(let error):
+                return .failure(error)
+            case .success(let results):
+                accession_numbers.append(contentsOf: results)
+            }
+        }
+        
+        return .success(accession_numbers)
+    }
+    
+    public func ExtractFromTextWithOrganization(text: String, organization: Organization)  -> Result<[AccessionNumber], Error> {
+        
+        var accession_numbers = [AccessionNumber]()
+        
+        for p in organization.Patterns {
+            
+            let rsp = self.ExtractFromTextWithPattern(text: text, pattern: p)
+            
+            switch rsp {
+            case .failure(let error):
+                return .failure(error)
+            case .success(let results):
+                // append org name here
+                accession_numbers.append(contentsOf: results)
+            }
+        }
+        return .success(accession_numbers)
+    }
+
+    public func ExtractFromTextWithPattern(text: String, pattern: Pattern)  -> Result<[AccessionNumber], Error> {
+        
+        var accession_numbers = [AccessionNumber]()
+    
+        var re: NSRegularExpression
+        
+        do {
+            re = try NSRegularExpression(pattern: pattern.Pattern)
+        } catch (let error) {
+            return .failure(error)
+        }
+        
+        let range = NSRange(location: 0, length: text.utf16.count)
+        
+        let results = re.matches(in: text, options: [], range: range)
+        
+        for r in results {
+            
+            let a = AccessionNumber(Organization: "", Number: r.debugDescription)                
+            accession_numbers.append(a)
+        }
+        
+        return .success(accession_numbers)
+    }
+    
+    
 }
 
--- Tests/AccessionNumbersTests/AccessionNumbersTests.swift
+++ Tests/AccessionNumbersTests/AccessionNumbersTests.swift
@@ -6,6 +6,6 @@ final class AccessionNumbersTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(AccessionNumbers().text, "Hello, World!")
+        // XCTAssertEqual(AccessionNumbers().text, "Hello, World!")
     }
 }
